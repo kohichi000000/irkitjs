ef44504aeafa9bc0af421209045c0a67742c44cb
Shinichi Kogai <kogai0121@gmail.com>
2014-10-20 12:49:32 +0900

    add newrelic to heroku

--- web.js
+++ web.js
@@ -18,6 +18,10 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+// http.createServer(app).listen(app.get('port'), function(){
+//   console.log("Express server listening on port " + app.get('port'));
+// });
+
+app.listen(app.get('port'), function () {
+	console.log("Express server is listening on port "+ app.get('port') + "...")
+})
